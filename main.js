a1927fde59ea2cd2220c95e1772026a79e856451
danielaconceicao <daniela.3597@gmail.com>
2024-10-08 15:48:44 +0200

    lamp exercise finished

---
+++ main.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btn');
+const image = document.getElementById('img')
+
+btn.addEventListener('click', () => {
+    image.src="img/yellow_lamp.png";
+   /*  console.log('fui cliacado') */
+});
